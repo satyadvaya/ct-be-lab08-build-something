2c5c61c5ab3c8b5d1981a394cfe81ea0eacc5c82
satyadvaya <satyadvaya@gmail.com>
2021-09-28 01:41:45 -0700

    failiing test POST a new favorite

---
+++ __tests__/favorites.test.js
@@ -0,0 +1,41 @@
+const pool = require('../lib/utils/pool.js');
+const twilio = require('twilio');
+const setup = require('../data/setup.js');
+const request = require('supertest');
+const app = require('../lib/app.js');
+
+jest.mock('twilio', () => () => ({
+  messages: {
+    create: jest.fn(),
+  },
+}));
+
+describe('08_build-something user routes for favorite affirmations', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  beforeEach(() => {
+    return request(app).post('/api/v1/favorites').send({
+      user: 'Jack',
+      favoriteQuote:
+        'Your life is already a miracle of chance waiting for you to shape its destiny',
+    });
+  });
+
+  it('creates a new favorite affirmation associated with a user in our database and sends a text message', () => {
+    return request(app)
+      .post('/api/v1/favorites')
+      .send({
+        user: 'Jack',
+        favoriteQuote: 'You know more than you think',
+      })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: '2',
+          user: 'Jack',
+          favoriteQuote: 'You know more than you think',
+        });
+      });
+  });
+});
